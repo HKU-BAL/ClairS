6a217ec988b1a8ba227a2e8d722f01d2d7cd8cb7
zhengzhenxian <920596906@qq.com>
2021-10-23 13:51:39 +0800

    use vcf writer and reader to handle all vcf files

--- shared/vcf.py
+++ shared/vcf.py
@@ -2,10 +2,11 @@ import sys
 import shlex
 import os
 
+from textwrap import dedent
 from subprocess import PIPE
 from argparse import ArgumentParser
 from collections import defaultdict
-from shared.utils import subprocess_popen, Position as Position
+from shared.utils import subprocess_popen, Position as Position, file_path_from
 
 class TruthStdout(object):
     def __init__(self, handle):
@@ -13,6 +14,84 @@ class TruthStdout(object):
 
     def __del__(self):
         self.stdin.close()
+
+
+class VcfWriter(object):
+    def __init__(self, vcf_fn, ctg_name=None, ref_fn = None, write_header=True, show_ref_calls=False):
+        self.vcf_fn = vcf_fn
+        self.show_ref_calls = show_ref_calls
+        self.vcf_writer = open(self.vcf_fn, 'w')
+        self.ref_fn = ref_fn
+        self.ctg_name = ctg_name
+        if write_header:
+            self.write_header(ref_fn=ref_fn)
+
+    def close(self):
+        try:
+            self.vcf_writer.close()
+        except:
+            pass
+
+    def write_header(self, ctg_name=None, ref_fn=None, sample_name="SAMPLE"):
+        header = dedent("""\
+                    ##fileformat=VCFv4.2
+                    ##FILTER=<ID=PASS,Description="All filters passed">
+                    ##FILTER=<ID=LowQual,Description="Low quality variant">
+                    ##FILTER=<ID=RefCall,Description="Reference call">
+                    ##INFO=<ID=P,Number=0,Type=Flag,Description="Result from pileup calling">
+                    ##INFO=<ID=F,Number=0,Type=Flag,Description="Result from full-alignment calling">
+                    ##FORMAT=<ID=GT,Number=1,Type=String,Description="Genotype">
+                    ##FORMAT=<ID=GQ,Number=1,Type=Integer,Description="Genotype Quality">
+                    ##FORMAT=<ID=DP,Number=1,Type=Integer,Description="Read Depth">
+                    ##FORMAT=<ID=AD,Number=R,Type=Integer,Description="Read depth for each allele">
+                    ##FORMAT=<ID=PL,Number=G,Type=Integer,Description="Phred-scaled genotype likelihoods rounded to the closest integer">
+                    ##FORMAT=<ID=AF,Number=1,Type=Float,Description="Estimated allele frequency in the range of [0,1]">
+                    """
+               )
+
+        if self.ref_fn is not None:
+            reference_index_file_path = file_path_from(self.ref_fn, suffix=".fai", exit_on_not_found=True, sep='.')
+            with open(reference_index_file_path, "r") as fai_fp:
+                for row in fai_fp:
+                    columns = row.strip().split("\t")
+                    contig_name, contig_size = columns[0], columns[1]
+                    if self.ctg_name is not None and contig_name != self.ctg_name:
+                        continue
+                    header += "##contig=<ID=%s,length=%s>\n" % (contig_name, contig_size)
+
+        header += '#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\t%s\n' % (sample_name)
+
+        self.vcf_writer.write(header)
+
+    def write_row(self, POS, REF, ALT, QUAL, GT, DP, AF, CHROM=None, GQ=None, ID='.', FILTER="PASS", NAF=None, TAF=None, VT=None):
+        GQ = GQ if GQ else QUAL
+        CHROM = CHROM if CHROM else self.ctg_name
+        if not self.show_ref_calls and (GT == "0/0" or GT == "./."):
+            return
+        INFO = "GT:GQ:DP:AF"
+        FORMAT = "%s:%d:%d:%.4f" % (GT, GQ, DP, AF)
+        basic_vcf_format = "%s\t%d\t%s\t%s\t%s\t%d\t%s" % (
+            CHROM,
+            int(POS),
+            ID,
+            REF,
+            ALT,
+            QUAL,
+            FILTER,
+            )
+        if NAF is not None:
+            INFO += ":NAF"
+            FORMAT += ":%.4f" % (NAF)
+        if TAF is not None:
+            INFO += ":TAF"
+            FORMAT += ":%.4f" % (TAF)
+        if VT is not None:
+            INFO += ":VT"
+            FORMAT += ":%s" % (VT)
+
+        vcf_format = '\t'.join([basic_vcf_format, INFO, FORMAT]) + '\n'
+
+        self.vcf_writer.write(vcf_format)
 
 class VcfReader(object):
     def __init__(self, vcf_fn, ctg_name, ctg_start=None, ctg_end=None, is_var_format=True, is_happy_format=False, is_fp=None):
