fc98cd63f6f2566f1a212bdfd5e655ac02a42274
zhengzhenxian <920596906@qq.com>
2021-11-09 22:58:39 +0800

    update max coverage

--- src/mix_chunk_bam.py
+++ src/mix_chunk_bam.py
@@ -24,17 +24,20 @@ def get_coverage(coverage_log, ctg_name=None):
         coverage = int(ctg_row[0].split()[3])
     return coverage
 
-def check_max_sampled_coverage(nor_cov, tum_cov, pro, pair_gamma=0.5, min_coverage=4):
-    nor_cov = int(nor_cov / min_coverage * min_coverage)
-    tum_cov = int(tum_cov / min_coverage * min_coverage)
-    synthetic_coverage_1 = int(tum_cov / pro)
-    synthetic_coverage_2 = int(nor_cov / (1 + pair_gamma - pro))
+def check_max_sampled_coverage(nor_cov, tum_cov, synthetic_proportion, pair_gamma=0.5, min_coverage=4):
+    # nor_cov = int(nor_cov / min_coverage * min_coverage)
+    # tum_cov = int(tum_cov / min_coverage * min_coverage)
+    max_synthetic_coverage_for_tumor = int(tum_cov / synthetic_proportion)
+    max_synthetic_coverage_for_normal = int(nor_cov / (1 + pair_gamma - synthetic_proportion))
 
-    synthetic_coverage = min(synthetic_coverage_1, synthetic_coverage_2)
-    synthetic_coverage = int(synthetic_coverage / min_coverage * min_coverage)
+    max_synthetic_coverage = min(max_synthetic_coverage_for_tumor, max_synthetic_coverage_for_normal)
+    max_synthetic_coverage = int(max_synthetic_coverage)
 
-    return synthetic_coverage
+    return max_synthetic_coverage
 
+def random_sample(population, k):
+    random.seed(0)
+    return random.sample(population, k)
 
 def MixBin(args):
     normal_bam_fn = args.normal_bam_fn
@@ -42,76 +45,81 @@ def MixBin(args):
     output_fn = args.output_fn
     input_dir = args.input_dir
     cov_dir = args.cov_dir
-    ctg_name = args.ctgName
+    ctg_name = args.ctg_name
     samtools_execute_command = args.samtools
     samtools_threads = args.samtools_threads
     tensor_sample_mode = args.tensor_sample_mode
     min_coverage = args.minCoverage
     synthetic_proportion = args.synthetic_proportion
-    synthetic_depth = args.synthetic_depth
+    synthetic_coverage = args.synthetic_coverage
     contaminative_proportions = args.contaminative_proportions
     platform = args.platform
-    normal_depth_log = os.path.join(cov_dir, 'normal_' + ctg_name + cov_suffix)
-    tumor_depth_log = os.path.join(cov_dir, 'tumor_' + ctg_name + cov_suffix)
-    normal_bam_depth = get_coverage(normal_depth_log)
-    tumor_bam_depth = get_coverage(tumor_depth_log)
+    normal_coverage_proportion = args.normal_coverage_proportion
+    normal_coverage_log = os.path.join(cov_dir, 'raw_normal_' + ctg_name + cov_suffix)
+    tumor_coverage_log = os.path.join(cov_dir, 'raw_tumor_' + ctg_name + cov_suffix)
+    normal_bam_coverage = get_coverage(normal_coverage_log)
+    tumor_bam_coverage = get_coverage(tumor_coverage_log)
 
-    normal_bin_num = int(int(normal_bam_depth) / int(min_coverage))
-    tumor_bin_num = int(int(tumor_bam_depth) / int(min_coverage))
+    normal_bin_num = int(int(normal_bam_coverage) / int(min_coverage))
+    tumor_bin_num = int(int(tumor_bam_coverage) / int(min_coverage))
 
     bam_list = os.listdir(input_dir)
     normal_bam_list = [bam for bam in bam_list if bam.startswith('normal_' + ctg_name + '_')]
-    tumor_bam_list = [bam for bam in bam_list if bam.startswith('tumor_' + ctg_name  + '_')]
+    tumor_bam_list = [bam for bam in bam_list if bam.startswith('tumor_' + ctg_name + '_')]
+    assert len(normal_bam_list) == normal_bin_num
+    assert len(tumor_bam_list) == tumor_bin_num
 
-    max_syn_depth = check_max_sampled_coverage(nor_cov=normal_bam_depth, tum_cov=tumor_bam_depth,
-                                               pro=synthetic_proportion, pair_gamma=1.0, min_coverage=4)
+    max_syn_coverage = check_max_sampled_coverage(nor_cov=normal_bam_coverage, tum_cov=tumor_bam_coverage,
+                                               synthetic_proportion=synthetic_proportion, pair_gamma=normal_coverage_proportion, min_coverage=4)
 
-    if synthetic_depth is None:
-        synthetic_depth = max_syn_depth
-    elif synthetic_depth > max_syn_depth:
-        print('[WARNING] Synthetic depth is larger than maximum depth {} > {}'.format(synthetic_depth, max_syn_depth))
+    if synthetic_coverage is None:
+        print('[INFO] Synthetic coverage not set, use maximum synthetic coverage {}'.format(max_syn_coverage))
+        synthetic_coverage = max_syn_coverage
+    elif synthetic_coverage > max_syn_coverage:
+        synthetic_coverage = max_syn_coverage
+        print('[WARNING] Synthetic coverage is larger than maximum coverage {} > {}'.format(synthetic_coverage, max_syn_coverage))
 
-    tumor_depth = int(synthetic_depth * synthetic_proportion)
-    normal_depth = int(synthetic_depth - tumor_depth)
-    sampled_normal_bin_num = int(normal_depth / min_coverage)
-    sampled_tumor_bin_num = int(tumor_depth / min_coverage)
+    tumor_coverage = int(synthetic_coverage * synthetic_proportion)
+    normal_coverage = int(synthetic_coverage - tumor_coverage)
+    sampled_normal_bin_num = int(normal_coverage / min_coverage)
+    sampled_tumor_bin_num = int(tumor_coverage / min_coverage)
 
-    print ("[INFO] Normal depth/Tumor depth, normal sampled bins/tumor sampled bins:{}:{}".format(tumor_depth, normal_depth, sampled_normal_bin_num, sampled_tumor_bin_num))
-    sampled_normal_bam_list = [normal_bam_list[idx] for idx in random.sample(range(normal_bin_num), sampled_normal_bin_num)]
-    random.seed(0)
-    sampled_tumor_bam_list = [tumor_bam_list[idx] for idx in random.sample(range(tumor_bin_num), sampled_tumor_bin_num)]
+    sampled_normal_bam_list = [normal_bam_list[idx] for idx in random_sample(range(normal_bin_num), sampled_normal_bin_num)]
+    if tensor_sample_mode:
+        sampled_tumor_bam_list = tumor_bam_list
+    else:
+        sampled_tumor_bam_list = [tumor_bam_list[idx] for idx in random_sample(range(tumor_bin_num), sampled_tumor_bin_num)]
 
     # the rest sampled sample to generate normal bam
-    random.seed(0)
     rest_normal_bam_list = [normal_bam_fn for normal_bam_fn in normal_bam_list if normal_bam_fn not in sampled_normal_bam_list]
-    if sampled_normal_bin_num + sampled_tumor_bin_num <= len(rest_normal_bam_list):
-        pair_normal_bam_list = [rest_normal_bam_list[idx] for idx in random.sample(range(len(rest_normal_bam_list)), sampled_normal_bin_num + sampled_tumor_bin_num)]
+    tumor_all_bin_num = sampled_normal_bin_num + sampled_tumor_bin_num
+    normal_all_bin_num = int(tumor_all_bin_num * normal_coverage_proportion)
+    if normal_all_bin_num <= len(rest_normal_bam_list):
+        pair_normal_bam_list = [rest_normal_bam for rest_normal_bam in random_sample(rest_normal_bam_list, normal_all_bin_num)]
     else:
-        print('[WARNING] Need to resample normal bams!')
         # sample bin exhaust if pair normal coverage could not satisfy requirement
         # avoid to resample bin for model robustness
-        resampled_bin_count = sampled_normal_bin_num + sampled_tumor_bin_num - len(rest_normal_bam_list)
-        random.seed(0)
-        resample_bin_list = [sampled_normal_bam_list[idx] for idx in random.sample(range(len(sampled_normal_bam_list)), resampled_bin_count)]
+        print('[WARNING] Need to resample normal chunked BAMs!')
+        resampled_bin_count = normal_all_bin_num - len(rest_normal_bam_list)
+        resample_bin_list = [sampled_normal_bam_list[idx] for idx in random_sample(range(len(sampled_normal_bam_list)), resampled_bin_count)]
         pair_normal_bam_list = resample_bin_list + rest_normal_bam_list
 
+    print("[INFO] Raw normal BAM coverage/Raw tumor BAM coverage:{}x/{}x, normal sampled bins/tumor sampled bins:{}/{}".format(
+        normal_bam_coverage, tumor_bam_coverage, sampled_normal_bin_num, sampled_tumor_bin_num))
+    print("[INFO] Tumor sampled normal chunked BAMs coverage/bins:{}x/{}:{}".format(len(sampled_normal_bam_list) * min_coverage, len(sampled_normal_bam_list), ' '.join(sampled_normal_bam_list)))
+    print("[INFO] Tumor sampled tumor chunked BAMs coverage/bins:{}x/{}:{}".format(len(sampled_tumor_bam_list) * min_coverage, len(sampled_tumor_bam_list), ' '.join(sampled_tumor_bam_list)))
+    print("[INFO] Normal sampled BAMs coverage/bins: {}x/{}:{}".format(len(pair_normal_bam_list) * min_coverage, len(pair_normal_bam_list), ' '.join(pair_normal_bam_list)))
+    print("[INFO] Synthetic coverage: {}, Normal sampled BAMs intersection:{}\n".format(synthetic_coverage, set(pair_normal_bam_list).intersection(set(sampled_normal_bam_list + sampled_tumor_bam_list))))
 
-
-    print("[INFO] Normal coverage/tumor coverage: {}/{}, normal sampled bins/tumor sampled bins:{}/{}".format(
-        normal_coverage, tumor_coverage, sampled_normal_bin_num, sampled_tumor_bin_num))
-    print("[INFO] Tumor sampled normal chunked BAMs:{}:{}".format(len(sampled_normal_bam_list), ' '.join(sampled_normal_bam_list)))
-    print("[INFO] Tumor sampled tumor chunked BAMs:{}:{}".format(len(sampled_tumor_bam_list), ' '.join(sampled_tumor_bam_list)))
-    print("[INFO] Normal sampled BAMs:{}:{}".format(len(pair_normal_bam_list), ' '.join(pair_normal_bam_list)))
-    print("[INFO] Synthetic coverage: {}, Normal sampled BAMs intersection:{}".format(synthetic_coverage, set(pair_normal_bam_list).intersection(set(sampled_normal_bam_list + sampled_tumor_bam_list))))
-
-    tumor_sampled_bam_list = [bam for bam in sampled_normal_bam_list + sampled_tumor_bam_list]
+    tumor_sampled_bam_list = sampled_normal_bam_list + sampled_tumor_bam_list
     tumor_sampled_bam_list = ' '.join([os.path.join(input_dir, bam) for bam in tumor_sampled_bam_list])
+    tumor_output_bam = output_fn
 
     normal_output_bam = output_fn.replace('tumor_', 'normal_')
     normal_sampled_bam_list = ' '.join([os.path.join(input_dir, bam) for bam in pair_normal_bam_list])
 
-    subprocess_run(shlex.split("{} merge -f -@{} {} {}".format(samtools_execute_command, samtools_threads, output_fn, tumor_sampled_bam_list)))
-    subprocess_run(shlex.split("{} index -@{} {}".format(samtools_execute_command, samtools_threads,output_fn)))
+    subprocess_run(shlex.split("{} merge -f -@{} {} {}".format(samtools_execute_command, samtools_threads, tumor_output_bam, tumor_sampled_bam_list)))
+    subprocess_run(shlex.split("{} index -@{} {}".format(samtools_execute_command, samtools_threads, tumor_output_bam)))
 
     subprocess_run(shlex.split(
         "{} merge -f -@{} {} {}".format(samtools_execute_command, samtools_threads, normal_output_bam,
@@ -211,6 +219,9 @@ def main():
 
     parser.add_argument('--tensor_sample_mode', type=str2bool, default=0,
                         help="Add all tumor tensor and only sampling in tensor generation")
+
+    parser.add_argument('--normal_coverage_proportion', type=float, default=0.5,
+                        help="Normal synthetic normal and tumor pair proportion")
 
     args = parser.parse_args()
 
