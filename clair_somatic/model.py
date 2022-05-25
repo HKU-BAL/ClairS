e07b75dc0d0c6540978967f3bf883783415ce4e0
zhengzhenxian <920596906@qq.com>
2022-05-25 20:33:55 +0800

    add conv pileup and lstm pileup in torch model

--- clair_somatic/model.py
+++ clair_somatic/model.py
@@ -6,6 +6,8 @@ from einops import rearrange, repeat
 from einops.layers.torch import Rearrange
 import numpy as np
 import shared.param as param
+# helper methods
+
 def cal_scale(input_size, layers):
     output_size = input_size
     for _ in range(layers):
@@ -50,6 +52,166 @@ class BasicBlock(nn.Module):
 
     def forward(self, x):
         return nn.ReLU(inplace=True)(self.residual_function(x) + self.shortcut(x))
+
+
+class ResNet_pileup(nn.Module):
+
+    def __init__(self, apply_softmax=False, block=BasicBlock, num_block=1, num_classes=3, ):
+        super().__init__()
+
+        channel_size = param.pileup_channel_size
+        self.dim = channel_size * 2
+        in_channels = 4
+        conv1_channel_size = 16
+        conv2_channel_size = 32
+        conv3_channel_size = 64
+
+        self.conv1 = BasicConv2D(input_channel=in_channels, output_channel=conv1_channel_size, strides=2)
+
+        depth_scale_size = cal_scale(input_size=self.dim//in_channels, layers=3)
+        width_scale_size = cal_scale(input_size=param.no_of_positions, layers=3)
+
+        self.conv1_x = self._make_layer(block, conv1_channel_size, 1, 1)
+
+        self.conv2 = BasicConv2D(input_channel=conv1_channel_size, output_channel=conv2_channel_size, strides=2)
+        self.conv2_x = self._make_layer(block, conv2_channel_size, 1, 1)
+
+        self.conv3 = BasicConv2D(input_channel=conv2_channel_size, output_channel=conv3_channel_size, strides=2)
+        self.conv3_x = self._make_layer(block, conv3_channel_size, 1, 1)
+
+        self.dropout = nn.Dropout(p=0.5)
+        # self.conv3_x = self._make_layer(block, 64, num_block[1], 2)
+        # self.conv4_x = self._make_layer(block, 256, num_block[2], 2)
+        # self.conv5_x = self._make_layer(block, 512, num_block[3], 2)
+        self.flatten = nn.Flatten()
+        self.fc1 = nn.Linear(conv3_channel_size * depth_scale_size * width_scale_size, 16)
+        self.fc2 = nn.Linear(16, num_classes)
+
+        self.apply_softmax = apply_softmax
+        if self.apply_softmax:
+            self.softmax = nn.Softmax(dim=-1)
+
+    def _make_layer(self, block, out_channels, num_blocks, stride):
+        """make resnet layers(by layer i didnt mean this 'layer' was the
+        same as a neuron netowork layer, ex. conv layer), one layer may
+        contain more than one residual block
+        Args:
+            block: block type, basic block or bottle neck block
+            out_channels: output depth channel number of this layer
+            num_blocks: how many blocks per layer
+            stride: the stride of the first block of this layer
+        Return:
+            return a resnet layer
+        """
+
+        # we have num_block blocks per layer, the first block
+        # could be 1 or 2, other blocks would always be 1
+        strides = [stride] #+ [1]
+        layers = []
+        for stride in strides:
+            layers.append(block(out_channels, out_channels, stride))
+            self.in_channels = out_channels * block.expansion
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        input = torch.reshape(x, (-1, param.no_of_positions, self.dim // 4, 4))
+        input = input.permute(0, 3, 1, 2)
+        output = self.conv1(input)
+        output = self.conv1_x(output)
+        output = self.conv2(output)
+        output = self.conv2_x(output)
+        output = self.conv3(output)
+        output = self.conv3_x(output)
+
+        output = self.dropout(output)
+        output = self.flatten(output)
+        # output = self.conv3_x(output)
+        # output = self.conv4_x(output)
+        # output = self.conv5_x(output)
+        # output = self.avg_pool(output)
+        output = output.view(output.size(0), -1)
+        output = self.fc1(output)
+        output = self.fc2(output)
+
+        if self.apply_softmax:
+            output = self.softmax(output)
+        return output
+
+class bigru(nn.Module):
+    def __init__(
+        self,
+        num_classes=3,
+        width = param.no_of_positions,
+        # dim = param.channel_size,
+        batch_first=True,
+        apply_softmax=False
+    ):
+        super().__init__()
+        kwargs = dict(locals())
+
+        self.num_layers = 2
+        self.flatten = nn.Flatten()
+        self.lstm_hidden_size = 128
+        self.lstm_hidden_size2 = 160
+        self.input_shape = [param.no_of_positions, self.lstm_hidden_size2 * 2]
+        # self.dropout = nn.Dropout(0.2)
+
+        channel_size = param.pileup_channel_size
+        self.dim = channel_size * 2
+        self.lstm = nn.LSTM(input_size=self.dim, hidden_size=self.lstm_hidden_size, batch_first=batch_first,
+                            num_layers=1, bidirectional=True)
+
+        self.lstm_2 = nn.LSTM(input_size=self.lstm_hidden_size*2, hidden_size=self.lstm_hidden_size2, batch_first=batch_first,
+                            num_layers=1, bidirectional=True)
+
+        fc_unit_1 = 128
+        self.dropout_fc1 = nn.Dropout(p=0.2)
+        # depth_scale_size = cal_scale(input_size=depth, layers=len(self.layers_prefix))
+        # width_scale_size = cal_scale(input_size=width, layers=len(self.layers_prefix))
+        # fc1_shape = dim * depth_scale_size * width_scale_size
+        self.fc1 = nn.Linear(in_features=self.input_shape[0] * self.input_shape[1], out_features=fc_unit_1)
+        self.dropout_fc2 = nn.Dropout(p=0.2)
+
+        self.fc2 = nn.Linear(in_features=fc_unit_1, out_features=fc_unit_1)
+        self.dropout = nn.Dropout(p=0.5)
+
+        self.fc3 = nn.Linear(in_features=fc_unit_1, out_features=num_classes)
+        self.selu = nn.SELU()
+        self.apply_softmax = apply_softmax
+        if self.apply_softmax:
+            self.softmax = nn.Softmax(dim=-1)
+    def forward(self, x):
+        # h0 = torch.randn(self.num_layers * 2, self.input_shape[0], self.input_shape[1])
+        # c0 = torch.randn(self.num_layers * 2, self.input_shape[0], self.input_shape[1])
+        # input = torch.reshape(x,(-1, param.no_of_positions, self.dim // 4, 4))
+        output, hidden = self.lstm(x)
+        # output = self.flatten(output)
+        # output = self.layer2(output)
+        # if len(self.layers_prefix) == 3:
+        #     output = self.layer3(output)
+        output, hidden = self.lstm_2(output)
+
+        output = self.flatten(output)
+        output = output.view(output.size(0), -1)
+        # output = self.dropout(output)
+        output = self.dropout_fc1(self.fc1(output))
+
+        output = self.dropout_fc2(self.fc2(output))
+        output = self.selu(self.fc3(output))
+
+        if self.apply_softmax:
+            output = self.softmax(output)
+        return output
+        # output = self.fc(output)
+        # return output
+        # return self.layers(x)
+
+
+#ResNet in Clair3
+import torch
+import torch.nn as nn
+
 
 class BottleNeck(nn.Module):
     """Residual block for resnet over 50 layers
