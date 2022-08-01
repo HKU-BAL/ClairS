31984b1bab0a006d2aa2fa158166d74ad39e8492
zhengzhenxian <920596906@qq.com>
2022-08-01 15:00:54 +0800

    update split bam submodule

--- src/split_bam.py
+++ src/split_bam.py
@@ -1,16 +1,15 @@
 import os
 import random
 import shlex
+
 from argparse import ArgumentParser, SUPPRESS
-import subprocess
-from subprocess import DEVNULL
 from subprocess import PIPE
+
+from src.utils import subprocess_popen
+
 random.seed(0)
-
-from src.utils import subprocess_popen, file_path_from, IUPAC_base_to_num_dict as BASE2NUM, region_from, \
-    reference_sequence_from, str2bool
-
 cov_suffix = ".cov.mosdepth.summary.txt"
+
 
 def get_coverage(coverage_log, ctg_name=None):
     # we use the overall average coverage if no contig specified
@@ -25,7 +24,8 @@ def get_coverage(coverage_log, ctg_name=None):
         coverage = int(ctg_row[0].split()[3])
     return coverage
 
-def SplitBin(args):
+
+def split_bin(args):
     normal_bam_fn = args.normal_bam_fn
     tumor_bam_fn = args.tumor_bam_fn
     output_dir = args.output_dir
@@ -33,10 +33,8 @@ def SplitBin(args):
     samtools_execute_command = args.samtools
     samtools_threads = args.samtools_threads
     samtools_output_threads = args.samtools_output_threads
-    min_coverage = args.minCoverage
-    proportion = args.proportion
+    min_coverage = args.min_coverage
     cov_dir = args.cov_dir
-    platform = args.platform
 
     normal_coverage_log = os.path.join(cov_dir, 'raw_normal_' + ctg_name + cov_suffix)
     tumor_coverage_log = os.path.join(cov_dir, 'raw_tumor_' + ctg_name + cov_suffix)
@@ -46,16 +44,20 @@ def SplitBin(args):
     normal_bin_num = int(int(normal_bam_coverage) / int(min_coverage))
     tumor_bin_num = int(int(tumor_bam_coverage) / int(min_coverage))
 
-    for bam_fn, bin_num, prefix in zip((normal_bam_fn, tumor_bam_fn), (normal_bin_num, tumor_bin_num), ("normal", 'tumor')):
+    for bam_fn, bin_num, prefix in zip((normal_bam_fn, tumor_bam_fn), (normal_bin_num, tumor_bin_num),
+                                       ("normal", 'tumor')):
         subprocess_list = []
         for bin_idx in range(bin_num):
             output_fn = os.path.join(output_dir, '_'.join([prefix, ctg_name, str(bin_idx)]) + '.bam')
             save_file_fp = subprocess_popen(
-                shlex.split("{} view -bh -@ {} - -o {}".format(samtools_execute_command,samtools_output_threads, output_fn)), stdin=PIPE,
+                shlex.split(
+                    "{} view -bh -@ {} - -o {}".format(samtools_execute_command, samtools_output_threads, output_fn)),
+                stdin=PIPE,
                 stdout=PIPE)
             subprocess_list.append(save_file_fp)
 
-        samtools_view_command = "{} view -@ {} -h {} {}".format(samtools_execute_command, samtools_threads, bam_fn, ctg_name if ctg_name else "")
+        samtools_view_command = "{} view -@ {} -h {} {}".format(samtools_execute_command, samtools_threads, bam_fn,
+                                                                ctg_name if ctg_name else "")
 
         samtools_view_process = subprocess_popen(shlex.split(samtools_view_command))
 
@@ -68,7 +70,7 @@ def SplitBin(args):
             # bin_id = int(random.random() * 100) % bin_num
             bin_id = row_id % bin_num
             # add prefix  for each normal and tumor reads
-            subprocess_list[bin_id].stdin.write(prefix[0]+row)
+            subprocess_list[bin_id].stdin.write(prefix[0] + row)
 
         samtools_view_process.stdout.close()
         samtools_view_process.wait()
@@ -76,53 +78,50 @@ def SplitBin(args):
             save_file_fp.stdin.close()
             save_file_fp.wait()
 
-    print ("[INFO] Contig/Normal coverage/Tumor coverage: {}/{}/{}".format(ctg_name, normal_bam_coverage, tumor_bam_coverage))
+    print("[INFO] Contig/Normal coverage/Tumor coverage: {}/{}/{}".format(ctg_name, normal_bam_coverage,
+                                                                          tumor_bam_coverage))
+
 
 def main():
-    parser = ArgumentParser(description="Generate variant candidate tensors using phased full-alignment")
-
-    parser.add_argument('--platform', type=str, default='ont',
-                        help="Sequencing platform of the input. Options: 'ont,hifi,ilmn', default: %(default)s")
-
-    parser.add_argument('--normal_bam_fn', type=str, default="input.bam",
-                        help="Sorted BAM file input, required")
-
-    parser.add_argument('--tumor_bam_fn', type=str, default="input.bam",
-                        help="Sorted BAM file input, required")
-
-    parser.add_argument('--normal_bam_coverage', type=int, default=None,
-                        help="Sorted BAM file input, required")
-
-    parser.add_argument('--tumor_bam_coverage', type=int, default=None,
-                        help="Sorted BAM file input, required")
-
-    parser.add_argument('--samtools', type=str, default="samtools",
-                        help="Path to the 'samtools', samtools version >= 1.10 is required. default: %(default)s")
+    parser = ArgumentParser(description="Split BAM into small bins")
 
     parser.add_argument('--output_dir', type=str, default=None,
-                        help="Sorted BAM file input, required")
+                        help="Sorted chunked BAM file output directory, required")
 
-    parser.add_argument('--cov_dir', type=str, default=None,
-                        help="Sorted BAM file input, required")
+    parser.add_argument('--normal_bam_fn', type=str, default=None,
+                        help="Sorted normal BAM file input, required")
 
-    parser.add_argument('--minCoverage', type=int, default=4,
-                        help="Reference fasta file input, required")
-
-    parser.add_argument('--samtools_threads', type=int, default=32,
-                        help="Reference fasta file input, required")
-
-    parser.add_argument('--samtools_output_threads', type=int, default=24,
-                        help="Reference fasta file input, required")
-
-    parser.add_argument('--proportion', type=float, default=0.1,
-                        help="Reference fasta file input, required")
+    parser.add_argument('--tumor_bam_fn', type=str, default=None,
+                        help="Sorted tumor BAM file input, required")
 
     parser.add_argument('--ctg_name', type=str, default=None,
-                        help="The name of sequence to be processed, required if --bed_fn is not defined")
+                        help="The name of sequence to be processed")
+
+    parser.add_argument('--normal_bam_coverage', type=int, default=None,
+                        help="Normal BAM coverage calculated using mosdepth")
+
+    parser.add_argument('--tumor_bam_coverage', type=int, default=None,
+                        help="Tumor BAM coverage calculated using mosdepth")
+
+    parser.add_argument('--samtools', type=str, default="samtools",
+                        help="Path to the 'samtools', samtools version >= 1.10 is required")
+
+    parser.add_argument('--cov_dir', type=str, default=None,
+                        help="Directory of mosdepth coverage summary")
+
+    parser.add_argument('--min_coverage', type=int, default=4,
+                        help="Minimum average coverage for each chunked bin")
+
+    parser.add_argument('--samtools_threads', type=int, default=32,
+                        help="Samtools threads to read input BAM")
+
+    parser.add_argument('--samtools_output_threads', type=int, default=24,
+                        help="Samtools threads to write input BAM")
+
 
     args = parser.parse_args()
 
-    SplitBin(args)
+    split_bin(args)
 
 
 if __name__ == "__main__":
