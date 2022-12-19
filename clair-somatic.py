d990168576c1ce39d850d7b716b3bd78d62167e6
zhengzhenxian <920596906@qq.com>
2022-12-19 15:39:56 +0800

    updated main entry

--- clair-somatic.py
+++ clair-somatic.py
@@ -3,60 +3,45 @@ from importlib import import_module
 
 DATA_PREP_SCRIPTS_FOLDER="src"
 DEEP_LEARNING_FOLDER="clair_somatic"
-POST_PROCESS_SCRIPTS_FOLDER="clair_somatic.metrics"
 
 deep_learning_folder = [
-    "CallVarBam",
-    "CallVariants",
     "train",
-    "Predict",
-    "Predict_with_logits",
-    "Train_multi-task",
-    "Predict_multi_class",
-    "Train_multi-task2",
-    "Predict_multi_class_add",
-    "Train_torch",
     "predict",
     "call_variants",
-    "call_variants_from_bam",
-    "Predict_all"
 ]
 
 REPO_NAME = "clair-somatic"
 data_preprocess_folder = [
     "get_candidates",
-    "extract_candidates_test",
     "create_tensor",
     "create_bin",
     "split_bam",
-    "PlotAlignment",
     "mix_chunk_bam",
     "extract_candidates",
-    "Vcf2Bed",
-    "PlotAF",
     "filter_reference_calls",
-    "create_tensor_adjacent",
     "create_tensor_pileup",
     'find_tumor_truth_in_normal',
     "update_variant",
     "create_pair_tensor",
     "sort_vcf",
     "compare_vcf",
-    "extract_candidates_with_af",
-    "extract_candidates_with_af_ru"
+    "create_pair_tensor_pileup",
+    "extract_pair_candidates",
+    'realign_reads',
+    'realign_variants',
+    'select_hetero_snp_for_phasing',
+    "gen_contaminated_bam",
+    "haplotype_filtering",
+    "merge_vcf",
+    'add_back_missing_variants_in_genotyping',
 ]
 
-post_process_scripts_folder = [
-    'GetOverallMetrics',
-]
 
 def directory_for(submodule_name):
     if submodule_name in deep_learning_folder:
         return DEEP_LEARNING_FOLDER
     if submodule_name in data_preprocess_folder:
         return DATA_PREP_SCRIPTS_FOLDER
-    if submodule_name in post_process_scripts_folder:
-        return POST_PROCESS_SCRIPTS_FOLDER
     return ""
 
 
@@ -64,15 +49,13 @@ def print_help_messages():
     from textwrap import dedent
     print(dedent("""\
         {0} submodule invocator:
-            Usage: python clair3.py [submodule] [options of the submodule]
+            Usage: python clair-somatic.py [submodule] [options of the submodule]
         Available data preparation submodules:\n{1}
         Available clair submodules:\n{2}
-        Available post processing submodules:\n{3}
         """.format(
             REPO_NAME,
             "\n".join("          - %s" % submodule_name for submodule_name in data_preprocess_folder),
             "\n".join("          - %s" % submodule_name for submodule_name in deep_learning_folder),
-            "\n".join("          - %s" % submodule_name for submodule_name in post_process_scripts_folder),
         )
     ))
 
@@ -85,15 +68,13 @@ def main():
     submodule_name = sys.argv[1]
     if (
         submodule_name not in deep_learning_folder and
-        submodule_name not in data_preprocess_folder and
-        submodule_name not in post_process_scripts_folder
+        submodule_name not in data_preprocess_folder
     ):
         sys.exit("[ERROR] Submodule %s not found." % (submodule_name))
 
     directory = directory_for(submodule_name)
     submodule = import_module("%s.%s" % (directory, submodule_name))
 
-    # filter arguments (i.e. filter clair3.py) and add ".py" for that submodule
     sys.argv = sys.argv[1:]
     sys.argv[0] += (".py")
 
