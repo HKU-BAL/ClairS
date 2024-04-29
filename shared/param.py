39ff5ff0f9097ca3c83326e254977735764cc5c4
zhengzhenxian <920596906@qq.com>
2024-04-29 19:00:13 +0800

    update main entry

--- shared/param.py
+++ shared/param.py
@@ -1,6 +1,6 @@
 # parameters
 caller_name = "clairs"
-version = "1.0.0"
+version = "0.2.0"
 
 from itertools import accumulate
 
@@ -117,9 +117,6 @@ NORMAL_PREFIX = 'n'
 TUMOR_PREFIX = 't'
 variant_type = {'ref', 'homo_somatic', 'homo_germline', 'hetero_germline'}
 grad_norm_clip = 1.0
-upper_beta = 6
-lower_beta = 4
-upper_beta_liqud = 5
 
 use_beta_subsampling = True
 use_exp_subsampling = False
